4fe8d95957acfd669df71ea947071b2e60f28787
Gabriel Niño <gabhack@gmail.com>
2025-07-28 17:32:50 -0500

    Versión para estudiantes sin tests ni .env

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv')
+const cors = require('cors')
+const api = require('./routes/api.routes')
+
+dotenv.config();
+const databaseConnect = require('./db/config')
+databaseConnect()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cors())
+app.use('/', api)
+
+module.exports = app
